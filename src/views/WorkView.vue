3b5bac5740734c3dfe5b3ca0fdf15d7321673c3e
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-09-25 10:31:58 +0300

    updated the work section to ensure the work details are up-to-date

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -3,6 +3,14 @@
 <template>
     <div class="work_container">
         <h1 class="title"><span class="brackets">&lt; My Work &gt;</span></h1>
+        
+        <div class="work_item">
+            <h2>Melodymix studio</h2>
+            <p>It's a website for selling shoes which is created using <span>(VueJs)</span></p>
+            <div class="link">
+                <a href="https://lawbertshoes.netlify.app" target="_blanck" >View</a>
+            </div>
+        </div>
 
         <div class="work_content">
             <div class="work_item">
@@ -21,13 +29,6 @@
                </div>
            </div>
 
-            <div class="work_item">
-                <h2>Melodymix studio</h2>
-                <p>It's a studio website with good features and design created using <span>(VueJs)</span></p>
-                <div class="link">
-                    <a href="https://melodymixstudio.netlify.app" target="_blanck" >View</a>
-                </div>
-            </div>
 
             <div class="work_item">
                 <h2>LawnetEstate</h2>
